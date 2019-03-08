190ec2db20cf12052ab380eeca564d868ec5e8f4
meowcodes <meowcodes@Sams-iPhone.localdomain>
2019-03-08 11:17:26 -0800

    add save reservation method

--- models/reservation.js
+++ models/reservation.js
@@ -69,6 +69,19 @@ class Reservation {
 
     return results.rows.map(row => new Reservation(row));
   }
+
+  /** save a reservation */
+  async save() {
+    // add rsv to database
+    const result = await db.query(`
+      INSERT INTO reservations 
+        (customer_id, start_at, num_guests, notes)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id`, 
+      [this.customerId, this.startAt, this.numGuests, this.notes]
+    )
+    this.id = result.rows[0].id;    
+  }
 }
 
 
